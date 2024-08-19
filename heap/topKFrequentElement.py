dbebf95a90d89b6126a6831e29c6a1a3d354cabc
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2024-08-19 22:18:18 +0530

    Heap learning in progress

---
+++ heap/topKFrequentElement.py
@@ -0,0 +1,15 @@
+# The parent node in a heap should have minimum value and that should be a complete binary tree
+from collections import Counter
+def findTopKElements(arr):
+
+
+
+
+
+
+# Driver code
+arr = [1, 1, 1, 1, 2, 2, 3, 3]
+k = 2
+
+# Result
+result = findTopKElements(arr, k)
